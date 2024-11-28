fd5d95d61e56da2fe28f438016bb4359e920ceb4
Gabriel Cerdio <yo_mero_yo@hotmail.com>
2024-11-27 22:29:28 -0600

    move folders

--- Pro_portfolio/src/main.jsx
+++ Pro_portfolio/src/main.jsx
@@ -2,9 +2,9 @@ import App from './App';
 import ReactDOM from 'react-dom/client';
 
 export default App;
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+<React.StrictMode>
     <App />
   </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
 );
